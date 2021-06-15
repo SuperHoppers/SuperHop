207acb2982b11fbb4b86e6102eae0ff2822fade1
Daniella Aceves <daniella.aceves98@gmail.com>
2021-06-15 14:38:29 -0700

    {feat}: express for get  all products and single product

---
+++ server/api/products.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const { models: { Product }} = require('../db')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+})

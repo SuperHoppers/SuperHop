c817ccd2920e1ea47ac0bd724098d58ab82c5b06
itzsarahhx3 <sarahzhan522@gmail.com>
2021-06-15 18:15:00 -0400

    {feat} redux for products/product

---
+++ client/store/products.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+// ACTION TYPES
+const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+const GET_SINGLE_PRODUCT = "GET_SINGLE_PRODUCT";
+
+// ACTION CREATORS
+const setProducts = (products) => {
+    return {
+        type: GET_ALL_PRODUCTS,
+        products
+    };
+};
+
+const setSingleProducts = (product) => {
+    return {
+        type: GET_SINGLE_PRODUCT,
+        product
+    };
+};
+
+// THUNK
+export const fetchAllProducts = () => async (dispatch) => {
+    try {
+        const { data } = await axios.get("/api/products");
+        dispatch(setProducts(data));
+    } catch (error) {
+        console.log("error fetching all products", err);
+    }
+};
+
+export const fetchSingleProduct = () => async (dispatch) => {
+    try {
+        const { data } = await axios.get(`/api/products/${productId}`);
+        dispatch(setSingleProducts(data));
+    } catch (error) {
+        console.log("error fetching single product", err);
+    }
+};
+
+// initial state
+const initialState = {
+    allProducts: [],
+    singleProduct: {}
+};
+
+// REDUCER
+const productsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ALL_PRODUCTS:
+            return { ...state, allProducts: action.products };
+        case GET_SINGLE_PRODUCT:
+            return { ...state, singleProduct: action.product };
+        default:
+            return state;
+    }
+};
+
+export default productsReducer;

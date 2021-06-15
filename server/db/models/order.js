cbc18f958e6eacb54ac18904ce22bd70d0f3d0f8
Regina Kim <kimregina2757@gmail.com>
2021-06-15 15:15:14 -0400

    edited Order model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,20 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('./database');
 
 const Order = db.define('order', {
-  id: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  customerId: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
   totalCost: {
     type: Sequelize.FLOAT,
     defaultValue: 0.0,
@@ -23,10 +9,6 @@ const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM('open', 'closed'),
     defaulValue: 'open',
-  },
-  itemsInOrder: {
-    type: Sequelize.ARRAY(Sequelize.STRING),
-    // type: Sequelize.INTEGER
   },
 });
 

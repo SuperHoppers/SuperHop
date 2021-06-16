ada14c90846474a3780fce788a5a1c4a8006999a
itzsarahhx3 <sarahzhan522@gmail.com>
2021-06-16 16:45:13 -0400

    {feat}: routes & products

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,48 +1,53 @@
 //check routes and filenames
-
-import React from 'react';
-import { connect } from 'react-redux';
-import EachProduct from './EachProduct';
-import { fetchAllProducts } from '../store/products';
+import React from "react";
+import { connect } from "react-redux";
+import EachProduct from "./EachProduct";
+import { fetchAllProducts } from "../store/products";
 
 /**
  * COMPONENT
  */
 class AllProducts extends React.Component {
-  componentDidMount() {
-    this.props.loadProducts();
-  }
+    componentDidMount() {
+        this.props.loadProducts();
+    }
 
-  render() {
-    return (
-      <div>
-        <h1 className="page-title">Superpowers Available for Purchase</h1>
-        <div>
-          {this.props.products.length > 0 ? (
-            this.props.products.map((product) => {
-              return (
-                <div className="productContainer" key={product.id}>
-                  <EachProduct product={product} />
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <h1 className="page-title">
+                    Superpowers Available for Purchase
+                </h1>
+                <div>
+                    {this.props.products.length > 0 ? (
+                        this.props.products.map((product) => {
+                            return (
+                                <div
+                                    className="productContainer"
+                                    key={product.id}
+                                >
+                                    <EachProduct product={product} />
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <h2>No products currently available.</h2>
+                    )}
                 </div>
-              );
-            })
-          ) : (
-            <h2>No products currently available.</h2>
-          )}
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 const mapState = (state) => {
-  return {
-    products: state.allProducts,
-  };
+    return {
+        products: state.products.allProducts
+    };
 };
 
 const mapDispatch = (dispatch) => ({
-  loadProducts: () => dispatch(fetchAllProducts()),
+    loadProducts: () => dispatch(fetchAllProducts())
 });
 
 export default connect(mapState, mapDispatch)(AllProducts);

4411fd6ef7e29a0ea5d2bebcf1f4a301131a17e6
Rebekkah Jou <rebekkahniles@gmail.com>
2021-06-16 16:24:01 -0400

    {feat}: working on allProducts specs

--- client/components/AllProducts.spec.js
+++ client/components/AllProducts.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
+import mockAxios from './mock-axios';
 //import sinon from 'sinon';
 //import configureMockStore from 'redux-mock-store';
 //import thunkMiddleware from 'redux-thunk';
@@ -19,7 +20,9 @@ import { mount } from 'enzyme';
 
 import { fetchAllProducts } from '../store/products';
 
-import AllProducts from './AllProducts';
+import AllProducts, {
+  AllProducts as UnconnectedAllProducts,
+} from './AllProducts';
 
 const supernose = {
   id: 1,
@@ -57,81 +60,86 @@ const powers = [
       'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/c/c2/OoT_Bombchu_Render.png',
   },
 ];
-
-it('renders the campuses passed in as props', () => {
-  const wrapper = mount(
-    <AllProducts
-      products={powers}
-      // getCampuses={getCampusesSpy}
-    />
-  );
-  expect(wrapper.text()).to.include('SuperGreasy');
-  expect(wrapper.text()).to.include('SuperNose');
-  const images = wrapper.find('img').map((node) => node.get(0).props.src);
-  expect(images).to.include.members([
-    'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/c/c2/OoT_Bombchu_Render.png',
-  ]);
-});
-
-it('renders DIFFERENT products passed in as props', () => {
-  const wrapper = mount(
-    <AllProducts
-      products={[supernose]}
-      // getCampuses={getCampusesSpy}
-    />
-  );
-  expect(wrapper.text()).to.not.include('Forever Young');
-});
-
-describe('AllProducts view', () => {
-  it('renders a superpower name, image, and price passed in as state', () => {
-    const wrapper = mount(<AllProducts />);
-    expect(wrapper).to.include.text('Supernose');
-    expect(wrapper).to.include.text('.20');
+describe('Tier one: AllProducts view', () => {
+  beforeEach(() => {
+    mockAxios.onGet('/api/products').replyOnce(200, powers);
+  });
+  //line 65
+  it('renders the campuses passed in as props', () => {
+    const wrapper = mount(
+      <UnconnectedAllProducts
+        products={powers}
+        // getCampuses={getCampusesSpy}
+      />
+    );
+    expect(wrapper.text()).to.include('SuperGreasy');
+    expect(wrapper.text()).to.include('SuperNose');
+    const images = wrapper.find('img').map((node) => node.get(0).props.src);
+    expect(images).to.include.members([
+      'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/c/c2/OoT_Bombchu_Render.png',
+    ]);
   });
 
-  it('renders a list of products', () => {
-    const wrapper = mount(<AllProducts />);
-    expect(wrapper).to.include.text('Forever Young');
-    expect(wrapper).to.include.text('700');
-    expect(wrapper).to.include.text('SuperNose');
-    expect(wrapper).to.include.text('SuperGrease');
+  it('renders DIFFERENT products passed in as props', () => {
+    const wrapper = mount(
+      <AllProducts
+        products={[supernose]}
+        // getCampuses={getCampusesSpy}
+      />
+    );
+    expect(wrapper.text()).to.not.include('Forever Young');
   });
+
+  describe('AllProducts view', () => {
+    it('renders a superpower name, image, and price passed in as state', () => {
+      const wrapper = mount(<AllProducts />);
+      expect(wrapper).to.include.text('Supernose');
+      expect(wrapper).to.include.text('.20');
+    });
+
+    it('renders a list of products', () => {
+      const wrapper = mount(<AllProducts />);
+      expect(wrapper).to.include.text('Forever Young');
+      expect(wrapper).to.include.text('700');
+      expect(wrapper).to.include.text('SuperNose');
+      expect(wrapper).to.include.text('SuperGrease');
+    });
+  });
+
+  // describe('Connect: react-redux', () => {
+
+  //   it('initializes products from the server when the application loads the /products route', async () => {
+  //     const reduxStateBeforeMount = store.getState();
+  //     expect(reduxStateBeforeMount.allProducts).to.deep.equal([]);
+  //     mount(
+  //       <Provider store={store}>
+  //         <MemoryRouter initialEntries={['/allProducts']}>
+  //           <AllProducts />
+  //         </MemoryRouter>
+  //       </Provider>
+  //     );
+  //     await waitForExpect(() => {
+  //       const reduxStateAfterMount = store.getState();
+  //       expect(reduxStateAfterMount.allProducts).to.deep.equal(powers);
+  //     });
+  //   });
+
+  //   it('<AllProducts /> renders campuses from the Redux store', async () => {
+  //     const wrapper = mount(
+  //       <Provider store={store}>
+  //         <MemoryRouter initialEntries={['/campuses']}>
+  //           <AllProducts />
+  //         </MemoryRouter>
+  //       </Provider>
+  //     );
+  //     await waitForExpect(() => {
+  //       wrapper.update();
+
+  //       const { campuses: reduxCampuses } = store.getState();
+  //       reduxCampuses.forEach((reduxCampus) => {
+  //         expect(wrapper.text()).to.include(reduxCampus.name);
+  //       });
+  //     });
+  //   });
+  // });
 });
-
-// describe('Connect: react-redux', () => {
-
-//   it('initializes products from the server when the application loads the /products route', async () => {
-//     const reduxStateBeforeMount = store.getState();
-//     expect(reduxStateBeforeMount.allProducts).to.deep.equal([]);
-//     mount(
-//       <Provider store={store}>
-//         <MemoryRouter initialEntries={['/allProducts']}>
-//           <AllProducts />
-//         </MemoryRouter>
-//       </Provider>
-//     );
-//     await waitForExpect(() => {
-//       const reduxStateAfterMount = store.getState();
-//       expect(reduxStateAfterMount.allProducts).to.deep.equal(powers);
-//     });
-//   });
-
-//   it('<AllProducts /> renders campuses from the Redux store', async () => {
-//     const wrapper = mount(
-//       <Provider store={store}>
-//         <MemoryRouter initialEntries={['/campuses']}>
-//           <AllProducts />
-//         </MemoryRouter>
-//       </Provider>
-//     );
-//     await waitForExpect(() => {
-//       wrapper.update();
-
-//       const { campuses: reduxCampuses } = store.getState();
-//       reduxCampuses.forEach((reduxCampus) => {
-//         expect(wrapper.text()).to.include(reduxCampus.name);
-//       });
-//     });
-//   });
-// });

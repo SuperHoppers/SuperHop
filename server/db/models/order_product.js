3f88aecb8641055925b79d1c5416c277ecffaefa
Rebekkah Jou <rebekkahniles@gmail.com>
2021-06-16 17:41:22 -0400

    {refactor}: changes per Ben's code review

---
+++ server/db/models/order_product.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+//through table for many-many association
+const Order_Product = db.define('order_product', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
+  },
+  lineTotal: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+});

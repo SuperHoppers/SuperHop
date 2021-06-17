35e5478b426d42e7a8fba5ce59c55d4b1f612cc1
Regina Kim <kimregina2757@gmail.com>
2021-06-17 13:00:23 -0400

    add order_product instance method

--- server/db/models/order_product.js
+++ server/db/models/order_product.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const Product = require('./product');
 
 //through table for many-many association
 const Order_Product = db.define('order_product', {
@@ -9,7 +10,34 @@ const Order_Product = db.define('order_product', {
   },
   lineTotal: {
     type: Sequelize.INTEGER,
-    defaultValue: 0,
+    // defaultValue: 0,
+    // type: Sequelize.VIRTUAL,
+    // get() {
+    //   const productId = this.getDataValue('productId')
+    //   // const price =
+    //   return this.getDataValue('quantity') *
+    // }
+  },
+  hooks: {
+    beforeCreate: () => {
+      const product = Product.findByPk(this.productId);
+      const price = product.findPrice();
+      this.lineTotal = this.quantity * price;
+    },
+    beforeUpdate: () => {
+      const product = Product.findByPk(this.productId);
+      const price = product.findPrice();
+      this.lineTotal += this.quantity * price;
+    },
   },
 });
+
+Order_Product.prototype.findItemsInOrder = function (orderId) {
+  return this.findAll({
+    where: {
+      orderId: orderId,
+    },
+  });
+};
+
 module.exports = Order_Product;

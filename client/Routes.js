7661ac7bdff972e7a24f6d3c27fdcc0a436dd854
itzsarahhx3 <sarahzhan522@gmail.com>
2021-06-21 17:29:14 -0400

    {feat}: admin product list & route

--- client/Routes.js
+++ client/Routes.js
@@ -4,36 +4,53 @@ import { connect } from "react-redux";
 import Home from "./components/Home";
 import { me } from "./store";
 
-import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import AdminHome from './components/AdminHome';
-import Header from './components/Header';
-import AllProducts from './components/AllProducts';
-import SingleProduct from './components/SingleProduct';
-import Cart from './components/Cart';
-import { Login } from './components/LoginPage';
-import { Signup } from './components/SignupPage';
-import CheckoutPage from './components/CheckoutPage';
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import AdminHome from "./components/AdminHome";
+import Header from "./components/Header";
+import AllProducts from "./components/AllProducts";
+import SingleProduct from "./components/SingleProduct";
+import Cart from "./components/Cart";
+import { Login } from "./components/LoginPage";
+import { Signup } from "./components/SignupPage";
+import CheckoutPage from "./components/CheckoutPage";
+import AdminProductForm from "./components/AdminCreateProductForm";
+import AdminUpdateProductForm from "./components/AdminUpdateProductForm";
+import AdminProductList from "./components/AdminProductList";
 
 const Routes = () => {
-  return (
-    <Router>
-      <div className="overall-view">
-        <Header />
-      </div>
+    return (
+        <Router>
+            <div className="overall-view">
+                <Header />
+            </div>
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={AllProducts} />
-        <Route exact path="/products/:productId" component={SingleProduct} />
-        <Route path="/login" component={Login} />
-        <Route path="/admin" component={AdminHome} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/checkout" component={CheckoutPage} />
-      </Switch>
-    </Router>
-  );
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/products" component={AllProducts} />
+                <Route
+                    exact
+                    path="/products/:productId"
+                    component={SingleProduct}
+                />
+                <Route path="/login" component={Login} />
+                <Route exact path="/admin" component={AdminHome} />
+                <Route
+                    exact
+                    path="/admin/products"
+                    component={AdminProductList}
+                />
+                <Route
+                    exact
+                    path="/admin/products/create"
+                    component={AdminProductForm}
+                />
+                <Route path="/signup" component={Signup} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/checkout" component={CheckoutPage} />
+            </Switch>
+        </Router>
+    );
 };
 
 export default Routes;
--- client/components/AdminProductList.js
+++ client/components/AdminProductList.js
@@ -1,63 +1,71 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchAllProducts } from "../store/products";
 
-const products = [
-    {
-        id: 1,
-        name: "Fire",
-        price: 5000,
-        description: "ready to go FIRE!",
-        inventory: 500
-    },
-    {
-        id: 2,
-        name: "water",
-        price: 4000,
-        description: "BE WATER!",
-        inventory: 400
-    },
-    {
-        id: 3,
-        name: "ninja",
-        price: 3000,
-        description: "hahahahaha",
-        inventory: 300
+export class AdminProductList extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.handleDelete = this.handleDelete.bind(this);
+    // }
+
+    componentDidMount() {
+        this.props.loadProducts();
     }
-];
 
-const AdminProductList = (props) => {
-    return (
-        <div className="product__list">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Product ID</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Inventory</th>
-                        <th>Description</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
+    // handleDelete(id) {
+    //     this.props.deleteStudent(id);
+    // }
 
-                <tbody>
-                    {products.map((product) => (
-                        <tr key={product.id}>
-                            <td>{product.id}</td>
-                            <td>{product.name}</td>
-                            <td>{product.price}</td>
-                            <td>{product.inventory}</td>
-                            <td>{product.description}</td>
-                            <td>
-                                <button className="button">Edit</button>
+    render() {
+        console.log(this.props);
+        const { products } = this.props;
 
-                                <button className="button">Delete</button>
-                            </td>
+        return (
+            <div className="product__list">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Inventory</th>
+                            <th>Description</th>
+                            <th>Action</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+                    </thead>
+
+                    <tbody>
+                        {products.map((product) => (
+                            <tr key={product.id}>
+                                <td>{product.id}</td>
+                                <td>{product.name}</td>
+                                <td>{product.price}</td>
+                                <td>{product.inventory}</td>
+                                <td>{product.description}</td>
+                                <td>
+                                    <button className="button">Edit </button>
+
+                                    <button className="button">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+const mapState = (state) => {
+    return {
+        products: state.products.allProducts
+    };
 };
 
-export default AdminProductList;
+const mapDispatch = (dispatch) => {
+    return {
+        loadProducts: () => dispatch(fetchAllProducts())
+    };
+};
+
+export default connect(mapState, mapDispatch)(AdminProductList);

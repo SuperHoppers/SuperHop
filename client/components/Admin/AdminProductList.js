18de6b15592a561521074863621a1bfc9fe74f69
Regina Kim <kimregina2757@gmail.com>
2021-06-22 16:15:27 -0400

    (edit): adminUpdateForm

--- client/components/Admin/AdminProductList.js
+++ client/components/Admin/AdminProductList.js
@@ -1,71 +1,74 @@
-import React from "react";
-import { connect } from "react-redux";
-import { fetchAllProducts } from "../../store/products";
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchAllProducts } from '../../store/products';
+import { Link } from 'react-router-dom';
 
 export class AdminProductList extends React.Component {
-    // constructor() {
-    //     super();
-    //     this.handleDelete = this.handleDelete.bind(this);
-    // }
+  // constructor() {
+  //     super();
+  //     this.handleDelete = this.handleDelete.bind(this);
+  // }
 
-    componentDidMount() {
-        this.props.loadProducts();
-    }
+  componentDidMount() {
+    this.props.loadProducts();
+  }
 
-    // handleDelete(id) {
-    //     this.props.deleteStudent(id);
-    // }
+  // handleDelete(id) {
+  //     this.props.deleteStudent(id);
+  // }
 
-    render() {
-        console.log(this.props);
-        const { products } = this.props;
+  render() {
+    console.log(this.props);
+    const { products } = this.props;
 
-        return (
-            <div className="product__list">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Product ID</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Inventory</th>
-                            <th>Description</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
+    return (
+      <div className="product__list">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Product ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Inventory</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
 
-                    <tbody>
-                        {products.map((product) => (
-                            <tr key={product.id}>
-                                <td>{product.id}</td>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
-                                <td>{product.inventory}</td>
-                                <td>{product.description}</td>
-                                <td>
-                                    <button className="button">Edit </button>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.inventory}</td>
+                <td>{product.description}</td>
+                <td>
+                  <Link to={`/admin/products/${product.id}`}>
+                    <button className="button">Edit </button>
+                  </Link>
 
-                                    <button className="button">Delete</button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+                  <button className="button">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => {
-    return {
-        products: state.products.allProducts
-    };
+  return {
+    products: state.products.allProducts,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-    return {
-        loadProducts: () => dispatch(fetchAllProducts())
-    };
+  return {
+    loadProducts: () => dispatch(fetchAllProducts()),
+  };
 };
 
 export default connect(mapState, mapDispatch)(AdminProductList);

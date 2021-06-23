9bad9063a50ec0fae7bffc8f84a4da3545df8fee
Regina Kim <kimregina2757@gmail.com>
2021-06-23 15:26:58 -0400

    (fix): admin middleware

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -4,7 +4,7 @@ const {
 
 const requireToken = async (req, res, next) => {
   try {
-    // console.log('REQUIRETOKEN>>>', req.headers);
+    console.log('REQUIRETOKEN>>>', req.headers);
     const token = req.headers.authorization;
     // const token = window.localStorage.getItem('token');
     const user = await User.findByToken(token);
